38fd8c1707b2cbfb9acf5e67b9a5793f218d047a
AzhuraaaReyy <azhurareyy@gmail.com>
2025-03-20 23:30:04 +0700

    homepage3

--- src/Data/posts.jsx
+++ src/Data/posts.jsx
@@ -1,13 +1,13 @@
 const posts = [
   {
     id: 1,
-    image: "/public/images/britop.png",
+    image: "/images/britop.png",
 
     description: "Are you ready to feel the britpop vibes?",
   },
   {
     id: 2,
-    image: "/public/images/didknow.png",
+    image: "/images/didknow.png",
 
     description:
       "Apapun bahannya, saran mimin sih pilih yang menurut kalian nyaman.",
